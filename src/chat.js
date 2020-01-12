027429a4dbe43e8f4b232f5e4499b24ab0d68aca
Johanna Nilsson <nijodotcom@gmail.com>
2020-01-12 12:44:06 +0100

    messages renders to DOM one time

--- src/chat.js
+++ src/chat.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import io from "socket.io-client"; // använd i alla filer som använder biblioteket
-
 
 const socket = io('http://3.120.96.16:3000');
 // när chatvyn Inmount -> connection till socket och eventlyssnarna
@@ -11,39 +9,61 @@ const socket = io('http://3.120.96.16:3000');
 class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {msg: []};
   }
   componentDidMount() {
     var socket = io("http://3.120.96.16:3000");
     socket.on("connect", function() {
       console.log("Well done!");
     });
+    socket.on('new_message', message => {
+        console.log( message);
+     }); 
 
-    socket.on('messages', function(data){
-        console.log(data);
-      }); 
+     socket.on("messages", data => {
+      this.setState({ msg: data });
+      console.log(this.state);
+      console.log(data[0].username);
+    });
+     
   }
-
-
   render() {
     return (
       <>
-        <MsgBox />
+        <MsgBox msg={this.state.msg}/>
         <MyMsgBox />
       </>
     );
   }
 }
 
-
-class MsgBox extends React.Component{
-    render(){
-        return(
-            <div className={'MsgBox'}>
-                <span className={'userName'}>Username</span>
-                <p>Här ska meddelandenna vara</p>
-            </div>
-        );
+class MsgBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+ 
+    onChange(e){
+        this.setState({content: e.target.value} );
     }
+  
+  render() {
+    console.log(this.props.msg);
+    const msgList = this.props.msg.map(msg => {
+          return msg.username !== null ? (
+            <div className="MsgBox" key={msg.id}>
+              <span>User: {msg.username}</span>
+              <div>Sent:  {msg.content}</div>
+            </div>
+          ) : null;
+        });
+
+    return (
+
+    <div onChange={ this.onChange } value={ this.state}> {msgList}</div>
+
+    );
+  }
 }
 
 class MyMsgBox extends React.Component{
@@ -53,7 +73,7 @@ class MyMsgBox extends React.Component{
         this.onSubmit = this.onSubmit.bind(this);
         this.onChange = this.onChange.bind(this);
     }
-
+// Kan bara submita på btn. VARFÖR inte enter?
     onSubmit(e){
         e.preventDefault();
         this.setState(
@@ -64,11 +84,11 @@ class MyMsgBox extends React.Component{
             console.log(response);
         })
         );
+        
       }
+      // Lyssnar bara på textarean
       onChange(e){
           this.setState({content: e.target.value });
-          console.log(e.target.value);
-          console.log(typeof(e.target.value));
       }
     render(){
         return(
@@ -82,7 +102,6 @@ class MyMsgBox extends React.Component{
         );
     }
 }
-
 
 
 export default Chat
